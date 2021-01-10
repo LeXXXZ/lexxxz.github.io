4611e53c390005ed576c1b14564e667907691d57
Alexey Zimin <azimin@atypicaldigital.com>
2021-01-11 01:00:53 +0300

    add some js animation

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let hardSkills = [
+    "Good knowledge of Core and Enterprise Java.",
+    "Experience in Java EE.",
+    "Good knowledge of AEM development.",
+    "Experience in Spring Framework."
+];
+
+let skills = [
+    "Self-learning and responsible.",
+    "Good communication skills.",
+    "Good team player."
+];
+
+var typedOptions = {
+  loop: true,
+  loopCount: false,
+  startDelay: 100,
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 2000
+};
+
+var typedHardSkills = new Typed('.hard-skill-update', typedOptions);
+typedHardSkills.strings = hardSkills;
+
+var typedSkills = new Typed('.skill-update', typedOptions);
+typedSkills.strings = skills;
